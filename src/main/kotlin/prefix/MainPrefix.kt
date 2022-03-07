ac3255a9caf12a308841d88171957da5d2104287
Denisov Nikita <sollwarmorgrind@gmail.com>
2022-03-07 19:05:26 +0300

    Задание А - готово. Смотреть размерности входных данных. Как всегда TL

--- src/main/kotlin/prefix/MainPrefix.kt
+++ src/main/kotlin/prefix/MainPrefix.kt
@@ -1,25 +1,21 @@
 package prefix
 
-fun main() {
+fun main() { // Почему медленно u_u
     var input = readLine()!!.split(" ")
     val n = input[0].toInt()
     val q = input[1].toInt()
-    input = readLine()!!.split(" ")
-    val mas = IntArray(n)
-    repeat(n) {
-        mas[it] = input[it].toInt()
+    val mas = readLine()!!.trim().split(" ").map { it.toLong() } // LONG *#@#*!
+    val prefix = LongArray(mas.size + 1)
+    for (i in 1..n) {
+        prefix[i] = prefix[i-1] + mas[i-1]
     }
-    val prefMas = IntArray(mas.size + 1)
-    for (i in 1..mas.size) { // Заполняем префиксный массив
-        prefMas[i] = prefMas[i - 1] + mas[i - 1]
-    }
-
+    var l = 0
+    var r = 0
     repeat(q) {
         input = readLine()!!.split(" ")
-        val l = input[0].toInt() - 1 // Надо так
-        val r = input[1].toInt()
-
-        println(prefMas[r] - prefMas[l])
+        l = input[0].toInt() - 1
+        r = input[1].toInt()
+        println(prefix[r] - prefix[l])
     }
 }
 
